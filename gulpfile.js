8ab2bbd4b4e8c41b2dbb5f8c3c1a3e07e13f28cb
benwainwright <bwainwright28@gmail.com>
2016-03-22 12:19:22 +0000

    Frontend build environment setup with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
-var gulp  = require('gulp');
-var del   = require('del');
+"use strict";
+var gulp   = require("gulp");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var del    = require("del");
+var sass   = require("gulp-sass");
 
-gulp.task('clean', function() {
+var paths = {
+   scripts: "resources/scripts/*.js",
+   sass   : "resources/sass/*.scss"
+};
+
+gulp.task("clean", function() {
    return del([
-      '*.*~',
-      'libs/*.*~',
-   ]
+      "*.*~",
+      "libs/*.*~"
+   ]);
 });
 
-gulp.task("run", function() {
-   var main = new Main();
+gulp.task("scripts", function() {
+   return gulp.src(paths.scripts)
+      .pipe(concat("scripts.min.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("resources/scripts-dist"));
+});
+
+gulp.task("sass", function() {
+   return gulp.src(paths.sass)
+      .pipe(sass({outputStyle: "compressed"})
+      .on("error", sass.logError))
+      .pipe(gulp.dest("resources/styles"));
+});
+
+gulp.task("watch", function() {
+   gulp.watch(paths.scripts, ["scripts"]);
+   gulp.watch(paths.sass, ["sass"]);
 });
 
 gulp.task("default", function() {
+
 });
