5d2c439d24004c06c39a5d6bfaccd7d8a38301fd
benwainwright <bwainwright28@gmail.com>
2016-03-22 18:43:20 +0000

    Database skeleton

---
+++ libs/database.js
@@ -0,0 +1,27 @@
+module.exports = (function() {
+   "use strict";
+
+   function getSeries(id) {
+   }
+
+   function getMovie(id) {
+   }
+
+   function getEpisode(id) {
+   }
+
+   function getSeason(id) {
+   }
+
+   function getActor(id) {
+   }
+
+   function getGenre(id) {
+   }
+
+   function getScene(id) {
+   }
+
+   function getUser(id) {
+   }
+}());
---
+++ test/testDatabase.js
@@ -0,0 +1,9 @@
+var series = {
+   id      : 4,
+   title   : "Breaking Bad",
+   airDate : Date(),
+   synosis : "Description",
+   seasons : 5,
+   episodes:2015,
+   urlImage:"string..."
+};
