68a50afe5db5728d638d99ccdda0204dadfad3b7
benwainwright <bwainwright28@gmail.com>
2016-03-22 18:03:40 +0000

    Setup linting via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 "use strict";
+
 var gulp   = require("gulp");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
 var del    = require("del");
 var sass   = require("gulp-sass");
 var sm     = require("gulp-sourcemaps");
+var jshint = require("gulp-jshint");
+var jscs   = require("gulp-jscs");
 
 var paths = {
-   scripts: "resources/scripts/*.js",
-   sass   : "resources/sass/*.scss"
+   scripts   : "resources/scripts/",
+   sass      : "resources/sass/",
+   scriptdist: "resources/scripts-dist/",
+   libs      : "libs/",
+   handlers  : "pages/handlers/"
 };
 
 gulp.task("clean", function() {
@@ -18,13 +24,31 @@ gulp.task("clean", function() {
    ]);
 });
 
+gulp.task("lint", function() {
+   var src = [
+      "*.js",
+      paths.libs     + "*.js",
+      paths.handlers + "**/*.js"
+   ];
+
+   return gulp.src(src)
+      .pipe(jshint(".jshintrc"))
+      .pipe(jshint.reporter("jshint-stylish"))
+      .pipe(jscs())
+      .pipe(jscs.reporter());
+});
+
 gulp.task("scripts", function() {
    return gulp.src(paths.scripts)
       .pipe(sm.init())
+      .pipe(jshint(paths.scripts + ".jshintrc"))
+      .pipe(jshint.reporter("jshint-stylish"))
+      .pipe(jscs({configPath: paths.scripts + ".jscsrc"}))
+      .pipe(jscs.reporter())
       .pipe(concat("scripts.min.js"))
       .pipe(uglify())
       .pipe(sm.write("../scripts-maps"))
-      .pipe(gulp.dest("resources/scripts-dist"));
+      .pipe(gulp.dest(paths.scriptdist));
 });
 
 gulp.task("sass", function() {
@@ -35,10 +59,9 @@ gulp.task("sass", function() {
 });
 
 gulp.task("watch", function() {
-   gulp.watch(paths.scripts, ["scripts"]);
-   gulp.watch("resources/sass/**/*.scss", ["sass"]);
+   gulp.watch(paths.scripts + "*", ["scripts"]);
+   gulp.watch(paths.sass + "**/*.scss", ["sass"]);
 });
 
 gulp.task("default", function() {
-
 });
