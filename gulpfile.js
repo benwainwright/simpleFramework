c851a4cca8fdc05ab227952543a35196237ef5d1
benwainwright <bwainwright28@gmail.com>
2016-03-22 21:33:48 +0000

    setup a whole bunch of gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 "use strict";
 
-var gulp   = require("gulp");
-var uglify = require("gulp-uglify");
-var concat = require("gulp-concat");
-var del    = require("del");
-var sass   = require("gulp-sass");
-var sm     = require("gulp-sourcemaps");
-var jshint = require("gulp-jshint");
-var jscs   = require("gulp-jscs");
+var gulp       = require("gulp");
+var uglify     = require("gulp-uglify");
+var concat     = require("gulp-concat");
+var del        = require("del");
+var size       = require("gulp-filesize");
+var sass       = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var jshint     = require("gulp-jshint");
+var jscs       = require("gulp-jscs");
+var autopre    = require("gulp-autoprefixer");
+var autopoly   = require("gulp-autopolyfiller");
+var merge      = require("event-stream").merge;
+var order      = require("gulp-order");
+var uncss      = require("gulp-uncss");
+var nodemon    = require("gulp-nodemon");
+var mocha      = require("gulp-mocha");
 
 var paths = {
    scripts   : "resources/scripts/",
    sass      : "resources/sass/",
+   styles    : "resources/styles/",
    scriptdist: "resources/scripts-dist/",
    libs      : "libs/",
-   handlers  : "pages/handlers/"
+   handlers  : "pages/handlers/",
+   tests     : "test/"
 };
 
 var src = [
@@ -23,6 +33,33 @@ var src = [
    paths.handlers + "**/*.js"
 ];
 
+var browsers = ["last 2 versions", "ie 8", "ie 9"];
+
+var autoPreSettings = {
+   browsers: browsers
+};
+
+var autoPolySettings = {
+   browsers: browsers
+};
+
+var sassSettings = {
+   outputStyle: "compressed"
+};
+
+var noDemonSettings = {
+   script: "main.js",
+   watch : [
+      "libs",
+      "tests",
+      "pages",
+      "resources/scripts",
+      "resources/sass"
+   ],
+   ext   : "js json",
+   tasks : ["lint", "scripts", "sass"]
+};
+
 gulp.task("clean", function() {
    return del([
       "*.*~",
@@ -30,38 +67,64 @@ gulp.task("clean", function() {
    ]);
 });
 
+gulp.task("dev", function() {
+   nodemon(noDemonSettings);
+});
+
 gulp.task("lint", function() {
    return gulp.src(src)
       .pipe(jshint(".jshintrc"))
-      .pipe(jshint.reporter("jshint-stylish"))
+      .pipe(jshint.reporter())
       .pipe(jscs())
-      .pipe(jscs.reporter());
+      .pipe(jscs.reporter("console"));
+});
+
+gulp.task("test", function() {
+   return gulp.src(paths.tests + "*.js")
+      .pipe(mocha());
 });
 
 gulp.task("scripts", function() {
-   return gulp.src(paths.scripts)
-      .pipe(sm.init())
-      .pipe(jshint(paths.scripts + ".jshintrc"))
-      .pipe(jshint.reporter("jshint-stylish"))
-      .pipe(jscs({configPath: paths.scripts + ".jscsrc"}))
-      .pipe(jscs.reporter())
+   var allScripts = gulp.src(paths.scripts + "*.js")
+          .pipe(jshint(paths.scripts + ".jshintrc"))
+          .pipe(jscs({configpath: paths.scripts + ".jscsrc"}))
+          .pipe(jscs.reporter("inline"))
+          .pipe(size())
+          .pipe(concat("scripts.js"));
+
+   /* Add polyfills where necessary */
+   var polys = allScripts.pipe(autopoly("polyfills.js"),
+                               autoPolySettings)
+          .pipe(size());
+
+   /* Merge, map and uglify */
+   return merge(polys, allScripts)
+      .pipe(order(["polyfills.js", "scripts.js"]))
+      .pipe(sourcemaps.init())
       .pipe(concat("scripts.min.js"))
       .pipe(uglify())
-      .pipe(sm.write("../scripts-maps"))
+      .pipe(size())
+      .pipe(sourcemaps.write("../scripts.maps"))
       .pipe(gulp.dest(paths.scriptdist));
 });
 
 gulp.task("sass", function() {
-   return gulp.src(paths.sass)
-      .pipe(sass({outputStyle: "compressed"})
+   return gulp.src(paths.sass + "/*.scss")
+      .pipe(sass(sassSettings)
       .on("error", sass.logError))
-      .pipe(gulp.dest("resources/styles"));
+      .pipe(uncss())
+      .pipe(autopre(autoPreSettings))
+      .pipe(gulp.dest(paths.styles));
 });
 
 gulp.task("watch", function() {
-   gulp.watch(paths.scripts + "*", ["scripts"]);
+   var testWatch;
+   gulp.watch(paths.scripts + "*.js", ["scripts"]);
    gulp.watch(paths.sass + "**/*.scss", ["sass"]);
    gulp.watch(src, ["lint"]);
+   testWatch = src;
+   testWatch.push(paths.tests);
+   gulp.watch(testWatch, ["test"]);
 });
 
 gulp.task("default", function() {
