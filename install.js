fd55962ddd03b427e64905f590b7e9ba823fb7e3
benwainwright <bwainwright28@gmail.com>
2016-04-03 17:18:04 +0100

    removed all the stuff specific to the movie project

--- install.js
+++ install.js
@@ -5,43 +5,55 @@ var fs   = require("fs");
 var path = require("path");
 
 fs.stat("../../pages/handlers/index.js", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["pages/handlers/index.js", "../../"], function() {});
    }
 });
 
 fs.stat("../../pages/handlers/error.js", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["pages/handlers/error.js", "../../"], function() {});
    }
 });
 
 fs.stat("../../pages/handlers/notfound.js", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["pages/handlers/notfound.js", "../../"], function() {});
    }
 });
 
 fs.stat("../../pages/templates/index.html", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["pages/templates/index.html", "../../"], function() {});
    }
 });
 
 fs.stat("../../pages/templates/error.html", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["pages/templates/error.html", "../../"], function() {});
    }
 });
 
 fs.stat("../../pages/templates/notfound.html", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["pages/templates/notfound.html", "../../"], function() {});
    }
 });
 
+fs.stat("../../pages/partials/siteHeader.html", function(err, stats) {
+   if(stats == false || !stats.isFile()) {
+      copy(["pages/partials/siteHeader.html", "../../"], function() {});
+   }
+});
+
+fs.stat("../../pages/partials/siteFooter.html", function(err, stats) {
+   if(stats == false || !stats.isFile()) {
+      copy(["pages/partials/siteFooter.html", "../../"], function() {});
+   }
+});
+
 fs.stat("../../config.json", function(err, stats) {
-   if(!stats || !stats.isFile()) {
+   if(stats == false || !stats.isFile()) {
       copy(["config.default.json", "../../"], function() {
          fs.rename("../../config.default.json", "../../config.json", function() {});
       });
@@ -49,27 +61,26 @@ fs.stat("../../config.json", function(err, stats) {
 });
 
 fs.stat("../../resources", function(err, stats) {
-   console.log(stats);
-   if(!stats || !stats.isDir()) {
+   if(stats == false && !stats.isDir()) {
       fs.mkdirSync("../../resources");
    };
    fs.stat("../../resources/scripts", function(err, stats) {
-      if(!stats || !stats.isDir()) {
+      if(stats == false && !stats.isDir()) {
          fs.mkdirSync("../../resources/scripts");
       };
    });
    fs.stat("../../resources/styles", function(err, stats) {
-      if(!stats || !stats.isDir()) {
+      if(stats == false && !stats.isDir()) {
          fs.mkdirSync("../../resources/styles");
       };
    });
    fs.stat("../../resources/images", function(err, stats) {
-      if(!stats || !stats.isDir()) {
+      if(stats == false && !stats.isDir()) {
          fs.mkdirSync("../../resources/images");
       };
    });
    fs.stat("../../resources/scriptmaps", function(err, stats) {
-      if(!stats || !stats.isDir()) {
+      if(stats == false && !stats.isDir()) {
          fs.mkdirSync("../../resources/scriptmaps");
       };
    });
